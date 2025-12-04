a5ff19221a3d90445731a6512b53d4fb432eed6e
Brenda Sotelo Diosdado <desarrollador.jr.pech@gmail.com>
2025-12-04 13:35:12 -0600

    middleware en rutas de modulos

--- src/routes/modulos.route.js
+++ src/routes/modulos.route.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const modulos = require("../controllers/modulos.controller");
 const permissions = require('../utils/permissions');
 
-router.post('/new', modulos.create);
-router.post('/update/:id', modulos.update);
-router.post('/update-view/:id', modulos.updateView);
-router.post('/delete/:id', modulos.delete);
-router.get('/get-all', modulos.getAll);
+router.post('/new', permissions.isLoggedIn, modulos.create);
+router.post('/update/:id', permissions.isLoggedIn, modulos.update);
+router.post('/update-view/:id', permissions.isLoggedIn, modulos.updateView);
+router.post('/delete/:id', permissions.isLoggedIn, modulos.delete);
+router.get('/get-all', permissions.isLoggedIn, modulos.getAll);
 
 module.exports = router;
